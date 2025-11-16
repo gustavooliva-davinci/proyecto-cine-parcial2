68bdbbf5c6cff06caed9875042342a594fbfccd3
gusoliva85 <gusoliva85@gmail.com>
2025-11-16 02:20:07 -0300

    Modificacion visual

--- src/components/CineItem.jsx
+++ src/components/CineItem.jsx
@@ -4,8 +4,8 @@ function CineItem({ pelicula }) {
       <img src={pelicula.imagen} alt={pelicula.titulo} />
       <h3>{pelicula.titulo}</h3>
       <p>{pelicula.descripcion}</p>
-      <p><strong>Género:</strong> {pelicula.genero}</p>
-      <p><strong>Año:</strong> {pelicula.anio}</p>
+      <p><strong>Genero:</strong> {pelicula.genero} </p>
+      <p><strong>Año:</strong> {pelicula.anio} </p>
     </div>
   );
 }
